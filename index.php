a9592f47565efaeecaf39fa2db9552a4b8758f60
Antonio Jesus <abonbal829a@ieslacampina.es>
2023-06-07 16:36:08 +0200

    Añadimos al index la tabla y sus opciones

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+  //estableciendo conexion
+  require 'conexion.php';
+
+  //preparo la sentencia sql
+  $sql = "SELECT * FROM equipos order by Puntos DESC";
+
+  //ejeutando la sentencia y guardando el resultado
+  $resultado = $mysqli->query($sql);
+	
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Liga Santander</title>
+</head>
+<body>
+    <header>
+        <h1>Liga Santander</h1>
+    </header>
+        <h2>Tabla de Posiciones</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Equipo</th>
+                    <th>Puntos</th>
+                </tr>
+            </thead>
+            <tbody>
+					<?php
+					while($fila = $resultado->fetch_assoc()){
+						?>
+							<tr>
+								<td><?php echo $fila['Nombre'];?></td>
+								<td><?php echo $fila['Puntos'];?></td>					
+						<td><a href="Jugadores.php?id=<?php echo $fila["id_equipo"];?>"class="btn btn-danger">Jugadores</a></td>
+                        <td><a href="Estadio.php?id=<?php echo $fila["id_equipo"];?>"class="btn btn-danger">Estadios</a></td>
+							</tr>
+						<?php
+						}
+						?>
+					
+			</tbody>
+        </table>
+        
+    <footer>
+        <p>@2023 Liga Santander. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
