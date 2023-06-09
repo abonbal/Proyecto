cb0f8057fa83d44b3eb7de09fa7669c4e79abc2e
Antonio Jesus <abonbal829a@ieslacampina.es>
2023-06-09 08:33:02 +0200

    index. con bootstrap

--- index.php
+++ index.php
@@ -12,6 +12,55 @@
 <html>
 <head>
     <title>Liga Santander</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/jquery.dataTables.min.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        header {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        h1 {
+            margin: 0;
+        }
+
+        h2 {
+            margin-bottom: 10px;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+
+        thead {
+            background-color: #f2f2f2;
+        }
+
+        tbody tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+
+        a.btn {
+            margin-right: 5px;
+        }
+
+        footer {
+            text-align: center;
+            margin-top: 20px;
+            color: #888;
+        }
+    </style>
 </head>
 <body>
     <header>
@@ -32,9 +81,12 @@
 							<tr>
 								<td><?php echo $fila['Nombre'];?></td>
 								<td><?php echo $fila['Puntos'];?></td>					
-						<td><a href="Jugadores.php?id=<?php echo $fila["id_equipo"];?>"class="btn btn-danger">Jugadores</a></td>
-                        <td><a href="Estadio.php?id=<?php echo $fila["id_equipo"];?>"class="btn btn-danger">Estadios</a></td>
-                        <td><a href="Entrenadores.php?id=<?php echo $fila["id_equipo"];?>"class="btn btn-danger">Entrenadores</a></td>
+						<td><a href="Jugadores.php?id=<?php echo $fila["id_equipo"];?>"class="btn btn-primary">Jugadores</a></td>
+                        <td><a href="Estadio.php?id=<?php echo $fila["id_equipo"];?>"class="btn btn-success">Estadios</a></td>
+                        <td><a href="Entrenadores.php?id=<?php echo $fila["id_equipo"];?>"class="btn btn-info">Entrenadores</a></td>
+                        <td><a href="editar1.php?id=<?php echo $fila["id_equipo"];?>"class="btn btn-warning">Editar</a></td>
+                        <td><a href="eliminar.php?id=<?php echo $fila["id_equipo"];?>"class="btn btn-danger">Eliminar</a></td>
+
 							</tr>
 						<?php
 						}
@@ -42,9 +94,7 @@
 					
 			</tbody>
         </table>
-        <a href="añadir.php">Añadir equipo</a>
-        <a href="eliminar.php">eliminar equipo</a>
-        <a href="editar.php">editar equipo</a>
+        <a href="añadir1.php">Añadir equipo</a>
         
     <footer>
         <p>@2023 Liga Santander. Todos los derechos reservados.</p>
